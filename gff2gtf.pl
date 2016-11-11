b5b2ca86a17fdb2ba42e2e46877eeaf97929b0de
Paris Veltsos <parisveltsos@gmail.com>
2016-11-11 23:21:19 +0100

    add two files

---
+++ gff2gtf.pl
@@ -0,0 +1,26 @@
+##!/usr/bin/perl
+# downloaded from http://seqanswers.com/forums/showpost.php?p=55022&postcount=4
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use File::Basename;
+use Bio::FeatureIO;
+
+my $inFile = shift;
+my ($name, $path, $suffix) = fileparse($inFile, qr/\.gff/);
+my $outFile = $path . $name . ".gtf";
+
+my $inGFF = Bio::FeatureIO->new( '-file' => "$inFile",
+'-format' => 'GFF',
+'-version' => 3 );
+my $outGTF = Bio::FeatureIO->new( '-file' => ">$outFile",
+'-format' => 'GFF',
+'-version' => 2.5);
+
+while (my $feature = $inGFF->next_feature() ) {
+
+$outGTF->write_feature($feature);
+
+}

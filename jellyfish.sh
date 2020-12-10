f96e3d8161dbf14eb19fa8deca66537fada27927
Veltsos <parisveltsos@gmail.com>
2020-12-10 14:26:17 -0600

    made kmc work with variable input

--- jellyfish.sh
+++ jellyfish.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=jelly              # Job name
-#SBATCH --partition=sixhour           # Partition Name (Required)
+#SBATCH --job-name=jelly                # Job name
+#SBATCH --partition=sixhour               # Partition Name (Required) eeb sixhour
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
-#SBATCH -n 8                          # Run on a single CPU
-#SBATCH -N 8
-#SBATCH --mem=16gb                     # Job memory request
+#SBATCH --ntasks=1                          # Run on a single CPU
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=12gb                     # Job memory request
 #SBATCH --time=0-05:59:00             # Time limit days-hrs:min:sec
-#SBATCH --output=pblat_%j.log         # Standard output and error log
+#SBATCH --output=out_jelly_%j.log         # Standard output and error log
+
+# replaced by kmc.sh
+# for i in $(echo -e "IM125\nIM274\nSF\nIM62\nIM767"); do sbatch jellyfish.sh $i; done
 
 # instructions, and upload file to http://qb.cshl.edu/genomescope/
 
@@ -15,13 +18,13 @@ module load jellyfish
 
 echo "Running"
 
-cd /home/p860v026/scratch/kmer/Tolpis_coronop/Run2 
+cd /home/p860v026/temp/10x/$1
 
 pwd; hostname; date
 
-jellyfish count -m 21 <(gzip -dc *.gz) -o fastqR2all21.counts -C -s 2000000000 -U 500 -t 8 
+# jellyfish count -m 21 <(gzip -dc *fastq.gz) -o $1\_21.counts -C -s 2000000000 -U 500 -t 8 
 
-jellyfish histo -t 8 fastqR2all21.counts > fastqR2all21.counts.histo 
+jellyfish histo -t 8 $1\_21.counts > $1\_21.counts.histo 
 
 date
 
--- kmc.sh
+++ kmc.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=kmc                # Job name
-#SBATCH --partition=eeb               # Partition Name (Required) eeb sixhour
+#SBATCH --partition=sixhour               # Partition Name (Required) eeb sixhour
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
 #SBATCH --ntasks=1                          # Run on a single CPU
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=400gb                     # Job memory request
-#SBATCH --time=2-05:59:00             # Time limit days-hrs:min:sec
+#SBATCH --mem-per-cpu=20gb                     # Job memory request
+#SBATCH --time=0-05:59:00             # Time limit days-hrs:min:sec
 #SBATCH --output=out_kmc_%j.log         # Standard output and error log
+
+
+# for i in $(echo -e "IM125\nIM274\nSF\nIM62\nIM767"); do sbatch kmc.sh $i; done
 
 echo "Running"
 
-cd /home/p860v026/temp/kmer
+cd /home/p860v026/temp/10x/$1
 
 ## PART 1 - count kmers - 16 cpu 16 memory sixhour queue - 45 min run
 # this uses hard disk space instead of memory so it is best to work on the temp drive.
@@ -19,34 +22,34 @@ cd /home/p860v026/temp/kmer
 # mkdir tmp
 # ls *.fastq.gz > FILES
 # pwd; hostname; date
-# 
-# kmc -k21 -t16 -m4 -ci1 -fm -cs1000000 @FILES kmc_Mtilingii tmp
-# 
-# kmc_tools transform kmc_Mtilingii histogram kmc_Mtilingii_k21.hist -cx1000000
+
+# kmc -k21 -t16 -m4 -ci1 -fm -cs1000000 @FILES kmc_$1 tmp
+ 
+kmc_tools transform kmc_$1 histogram kmc_$1\_k21.hist -cx1000000
 
 ## PART 2 genomescope - 
-
-# module load R
 # 
-# genomescope.R -i kmc_Mtilingii_k21.hist -k 21 -p 2 -o genomescope_kmc_Mtilingii_k21_l10
-# 
-# tar -zcvf genomescope_kmc_Mtilingii_k21_l10.tar.gz genomescope_kmc_Mtilingii_k21_l10
+module load R
+ 
+genomescope.R -i kmc_$1\_k21.hist -k 21 -p 2 -o genomescope_kmc_$1_\k21
+ 
+tar -zcvf genomescope_kmc_$1_\k21.tar.gz genomescope_kmc_$1_\k21
 	
 ## PART 3 smudgeplot - 1 cpu 300 Mb memory eeb queue 12:30 hr runtime
 
-module load python
-module load R
-
-L=$(smudgeplot.py cutoff kmc_Mtilingii_k21.hist L)
-U=$(smudgeplot.py cutoff kmc_Mtilingii_k21.hist U)
-echo $L $U # these need to be sane values
-
-kmc_tools transform kmc_Mtilingii -ci"$L" -cx"$U" dump -s kmc_Mtilingii_L"$L"_U"$U".dump
-smudgeplot.py hetkmers -o kmc_Mtilingii_L"$L"_U"$U" < kmc_Mtilingii_L"$L"_U"$U".dump
-
-smudgeplot.py plot kmc_Mtilingii_L"$L"_U"$U"_coverages.tsv
-
-tar -zcvf smudgeplot_kmc_Mtilingii_k21.tar.gz smudgeplot_*
-
-date
-
+# module load python
+# module load R
+# 
+# L=$(smudgeplot.py cutoff kmc_Mtilingii2_k21.hist L)
+# U=$(smudgeplot.py cutoff kmc_Mtilingii2_k21.hist U)
+# echo $L $U # these need to be sane values
+# 
+# kmc_tools transform kmc_Mtilingii2 -ci"$L" -cx"$U" dump -s kmc_Mtilingii2_L"$L"_U"$U".dump
+# smudgeplot.py hetkmers -o kmc_Mtilingii2_L"$L"_U"$U" < kmc_Mtilingii2_L"$L"_U"$U".dump
+# 
+# smudgeplot.py plot kmc_Mtilingii2_L"$L"_U"$U"_coverages.tsv
+# 
+# tar -zcvf smudgeplot_kmc_Mtilingii2_k21.tar.gz smudgeplot_*
+# 
+# date
+# 

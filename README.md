3c5f365620cb5e16bbdcfa926277e2b152fd6f5a
Veltsos <parisveltsos@gmail.com>
2020-12-10 15:16:57 -0600

    update README to describe all files

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# shorts
 
-Backup of scripts while still in production. See text folder for more information on workflows.
+bwa.sh	Basic bwa alignment
+genomeStats.sh	makes STAR index which provides fasta length metrics
+htseq_count.sh	pipeline to trim, map 3prime RNAseq read data and combine to read table.
+jellyfish.sh	old way of kmer genome size estimation
+kmc.sh	new way of kmer genome size estimation, uses space rather than ram
+ngmlr.sh	aligner for SV with sniffles
+nucmer.sh	mummer SV identification from reads to genome mapping
+oneLiners.sh	bash common  bioinformatics manipulations
+pacbioSV.sh	approach to identify SV from pacbio mapping to assembly
+pblat.sh	align transcriptome to genome to manually check SV
+ragtag.sh	reads to genome for assembly correction and SV identification
+shorts.sh	Small scripts while still in production.
+supernova.sh	assemble 10x data
+trimmomatic.sh	basic script to trim reads
--- short.sh
+++ short.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=short              # Job name
+#SBATCH --job-name=gzip              # Job name
 #SBATCH --partition=sixhour           # Partition Name (Required)
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
-#SBATCH -n 8                          # Run on a single CPU
-#SBATCH -N 8
-#SBATCH --mem=2gb                     # Job memory request
-#SBATCH --time=0-03:59:00             # Time limit days-hrs:min:sec
-#SBATCH --output=pblat_%j.log         # Standard output and error log
+#SBATCH --ntasks=1                          # Run on a single CPU
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1gb                     # Job memory request
+#SBATCH --time=0-05:59:00             # Time limit days-hrs:min:sec
+#SBATCH --output=gzip_%j.log         # Standard output and error log
 
 echo "Running"
  
-cd /home/p860v026/Mimulus.genomes
-
+cd /home/p860v026/work 
 pwd; hostname; date
 
-COMMAND
- 
+# gzip JKK-LVR_MPS12342939_D10_7687_S1_L002_R2_001.fastq
+# gzip JKK-LVR_MPS12342939_D10_7687_S1_L002_R1_001.fastq
+# tar -zcf supernovaShort.tar.gz JKK-LVR3/ 
+
+cp -r /home/p860v026/work/Mimulus_tilingii_10x/* /home/p860v026/temp/Mimulus_tilingii_10x/
+cp -r /home/p860v026/work/pacbio/* /home/p860v026/temp/pacbio/
+cp -r /home/p860v026/work/tolpis_coronop/* /home/p860v026/temp/tolpis_coronop/
+
+
 date
 

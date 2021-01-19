64dd3d9bfe2708ecdd0eb8bc53b8973de70f6fe4
Veltsos <parisveltsos@gmail.com>
2021-01-19 11:48:36 -0600

    added project structure to pblat.sh - takes 1 week to do 4 chromosomes

--- pblat.sh
+++ pblat.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=pblat              # Job name
-#SBATCH --partition=sixhour           # Partition Name (Required)
+#SBATCH --partition=eeb           # Partition Name (Required)
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
-#SBATCH -n 8                          # Run on a single CPU
-#SBATCH -N 8
-#SBATCH --mem=2gb                     # Job memory request
-#SBATCH --time=0-05:59:00             # Time limit days-hrs:min:sec
+#SBATCH --ntasks=1                   # Run on a single CPU
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24gb                     # Job memory request
+#SBATCH --time=6-05:59:00             # Time limit days-hrs:min:sec
 #SBATCH --output=pblat_%j.log         # Standard output and error log
 
 echo "Running"
  
-cd /home/p860v026/Mimulus.genomes
+WORKFOLDER=/home/p860v026/temp/driver
+GENOMEFOLDER=/home/p860v026/temp/Mgutv5/assembly
+GENOME=MguttatusTOL_551_v5.0.fa
+QUERY=drive.seqs.v2build.fasta 
+ 
+cd $WORKFOLDER
 
-pwd; hostname; date
-
-mpirun ~/bin/icebert-pblat-cluster-f850247/pblat-cluster tilingii.fasta Mimulus_guttatus_TOL/annotation/MguttatusTOL_551_v5.0.transcript_primaryTranscriptOnly.fa primaryTranscriptOut.psl
+mpirun pblat-cluster $QUERY $GENOMEFOLDER/$GENOME $GENOME.$QUERY.psl
  
 date
 

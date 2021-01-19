a5da53009a54509b835589c9c2b78eade7bf9f02
Veltsos <parisveltsos@gmail.com>
2021-01-19 11:47:15 -0600

    add project folders to pacbioSV.sh - still does not find many inversions

--- pacbioSV.sh
+++ pacbioSV.sh
@@ -4,39 +4,48 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
 #SBATCH --ntasks=1                          # Run on a single CPU
-#SBATCH --cpus-per-task=8				# change THIS
-#SBATCH --mem-per-cpu=12gb                     # Job memory request
-#SBATCH --time=6-23:59:00             # Time limit days-hrs:min:sec
+#SBATCH --cpus-per-task=1				# change THIS
+#SBATCH --mem-per-cpu=8gb                     # Job memory request
+#SBATCH --time=4-23:59:00             # Time limit days-hrs:min:sec
 #SBATCH --output=out_pbSV_%j.log         # Standard output and error log
 
 echo "Running"
 
-cd /home/p860v026/temp/pacbioSV
+SMRTFLDR=/home/p860v026/temp/smrtlink/current/bundles/smrttools/smrtcmds/bin/
+PBNAME=767 # m64060_201112_195254 62 767 
+PBFOLDER=/home/p860v026/temp/pacbio/Lila.pacbio # /home/p860v026/temp/pacbio/r64060_20201112_192654/1_A01 /home/p860v026/temp/pacbio/Lila.pacbio
+GNMFOLDER=/home/p860v026/temp/Mgutv5/assembly
+OUTNAME=767 # 541 62 767
 	
-# make fastq files
+# PART 1. Make fastq files
 
-# 	/home/p860v026/temp/smrtlink/current/bundles/smrttools/smrtcmds/bin/bam2fastq -o allReadsAll /home/p860v026/temp/pacbio/r64060_20201112_192654/1_A01/m64060_201112_195254.subreads.bam	
+cd $PBFOLDER 
+
+$SMRTFLDR/bam2fastq -o $OUTNAME $PBFOLDER/$PBNAME.subreads.bam	
 
 ## Count number of reads
 
-# 	zcat allReads.fastq.gz | wc -l # 1340581
+# 	zcat $OUTNAME.fastq.gz | wc -l # 1340581
 	
-## Keep 10%
-# 	zcat allReads.fastq.gz | head -n above/4
 	
-# Align subset to chromosomes # 8 cpu
+# PART 2. Align to chromosomes # 8 cpu
 
-export TMPDIR=/home/p860v026/temp/pbmm2Temp
-
-/home/p860v026/temp/smrtlink/current/bundles/smrttools/smrtcmds/bin/pbmm2 align /home/p860v026/temp/Mgutv5/assembly/chr_10_13.fasta /home/p860v026/temp/pacbio/r64060_20201112_192654/1_A01/m64060_201112_195254.subreads.bam outBig.bam --sort --log-level INFO --log-level DEBUG --median-filter -j 8
+# export TMPDIR=/home/p860v026/temp/pbmm2Temp$OUTNAME
+# 
+# $SMRTFLDR/pbmm2 align $GNMFOLDER/MguttatusTOL_551_v5.0.fa /home/p860v026/temp/pacbio/62.bam out62.bam --sort --log-level INFO --log-level DEBUG --median-filter -j 8
 
 # SV discovery # 1 cpu
 
-/home/p860v026/temp/smrtlink/current/bundles/smrttools/smrtcmds/bin/pbsv discover outBig.bam outBig.svsig.gz --log-level INFO
+# $SMRTFLDR/pbsv discover out62.bam out62.svsig.gz --log-level INFO
 
 # Call SV and assign genotypes
 	
-	/home/p860v026/temp/smrtlink/current/bundles/smrttools/smrtcmds/bin/pbsv call /home/p860v026/temp/Mgutv5/assembly/chr_10_13.fasta outBig.svsig.gz outBig.var.vcf --log-level INFO
-	
+# $SMRTFLDR/pbsv call $GNMFOLDER/MguttatusTOL_551_v5.0.fa out62.svsig.gz out62.var.vcf --log-level INFO -j 8
+
+# Keep only mapped reads (only mapped reads are there)
+
+# samtools view -b -F 4 outAll.bam > outAll.mapped.bam
+# samtools index outAll.mapped.bam
+
 date
 

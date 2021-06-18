86665d1a25fbfe04b85a73f3857b52b66712db43
Veltsos <parisveltsos@gmail.com>
2021-06-18 17:00:53 -0500

    minor update busco

--- busco.sh
+++ busco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=busco               # Job name
+#SBATCH --job-name=bus1034              # Job name
 #SBATCH --partition=sixhour               # partition Name (Required) eeb sixhour
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
@@ -11,8 +11,9 @@
 
 BUSCOSUPPORT=/home/p860v026/temp/bin/buscoSupport
 
-GFOLDER=/home/p860v026/temp/pacbio/r64060_20201112_192654/1_A01/canu541
-GNAME=541.contigs.fasta
+GFOLDER=/home/p860v026/temp/IM1034/purge2/
+# GNAME=1034.contigs.fasta
+GNAME=1034purged2.fa
 
 module load busco
 
@@ -39,10 +40,3 @@ run_BUSCO.py -i $GNAME -o busco_$GNAME -l $BUSCOSUPPORT/embryophyta_odb9 -m geno
 
 # transcriptome
 # run_BUSCO.py -i SEQUENCE_FILE -o OUTPUT_NAME -l LINEAGE -m tran
-
-# results
-# C:91.6%[S:87.4%,D:4.2%],F:2.8%,M:5.6%,n:1440  V2
-# C:92.0%[S:87.5%,D:4.5%],F:1.5%,M:6.5%,n:1440	V5
-# C:88.8%[S:77.3%,D:11.5%],F:2.6%,M:8.6%,n:1440	canu62
-# C:88.7%[S:70.9%,D:17.8%],F:2.9%,M:8.4%,n:1440 canu767
-# C:93.8%[S:89.7%,D:4.1%],F:1.5%,M:4.7%,n:1440 541

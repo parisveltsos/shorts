fe7c0cb1e1f95d31499ec527eda7689c18ebaa9c
Veltsos <parisveltsos@gmail.com>
2021-07-14 14:54:30 -0500

    flo outputs to new folder zip files ready to download

--- flo.sh
+++ flo.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 #SBATCH --job-name=flo
-#SBATCH --partition=eeb      
+#SBATCH --partition=kelly      
 #SBATCH --time=1-05:59:00        
 #SBATCH --mem-per-cpu=2g
-#SBATCH --mail-user=pveltsos@ku.edu    
+#SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
 #SBATCH --ntasks=1             
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=18
 #SBATCH --output=flo.log
 
 cd /home/p860v026/temp/bin/flo/flo_mimulus
 
 rake -f Rakefile
+
+NAME=66421npto5_85
+
+cd run
+
+mv liftover.chn $NAME.chn
+
+gzip $NAME.chn
+
+cd Mguttatus_256_v2.0.gene/
+
+ruby ../../gff_recover.rb lifted.gff3 > lifted_cleaned.gff 2>lifted_rubbish.gff
+
+cd ..
+
+mv Mguttatus_256_v2.0.gene/ $NAME\_gff
+
+tar -zcvf $NAME\_gff.tar.gz $NAME\_gff/
+
+cp *.gz ../results/
 

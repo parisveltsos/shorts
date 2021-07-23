1e64ef8fd753e57d693887dddc2f333a58ff4742
Veltsos <parisveltsos@gmail.com>
2021-07-23 16:16:43 -0500

    minor flo update

--- flo.sh
+++ flo.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=flo
-#SBATCH --partition=kelly      
+#SBATCH --partition=eeb      
 #SBATCH --time=1-05:59:00        
-#SBATCH --mem-per-cpu=2g
-#SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
+#SBATCH --mem-per-cpu=24g
+#SBATCH --mail-user=pveltsos@ku.edu
 #SBATCH --ntasks=1             
-#SBATCH --cpus-per-task=18
+#SBATCH --cpus-per-task=10
 #SBATCH --output=flo.log
 
 cd /home/p860v026/temp/bin/flo/flo_mimulus
 
 rake -f Rakefile
 
-NAME=66421npto5_85
+NAME=1192to5_85
+GFFNAME=MguttatusTOL_551_v5.0.gene
 
 cd run
 
@@ -20,13 +21,13 @@ mv liftover.chn $NAME.chn
 
 gzip $NAME.chn
 
-cd Mguttatus_256_v2.0.gene/
+cd $GFFNAME
 
-ruby ../../gff_recover.rb lifted.gff3 > lifted_cleaned.gff 2>lifted_rubbish.gff
+ruby ../../gff_recover.rb lifted.gff3 > $NAME\lifted_cleaned.gff 2>$NAME\lifted_rubbish.gff
 
 cd ..
 
-mv Mguttatus_256_v2.0.gene/ $NAME\_gff
+mv $GFFNAME $NAME\_gff
 
 tar -zcvf $NAME\_gff.tar.gz $NAME\_gff/
 

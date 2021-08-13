b1cfd4897a9915ac36bf977e6c68389b9e40a377
Veltsos <parisveltsos@gmail.com>
2021-08-13 09:31:27 -0500

    update lepmap to automatically use line name

--- lepmap.sh
+++ lepmap.sh
@@ -10,59 +10,60 @@
 
 # make pedigree (based on key.txt, +1 for all key 1st column)
 # 	module load python/2.7
-# 	LINE=664
-# 	cd /home/p860v026/temp/3prime/mapped
-# 	mkdir $LINE
-# 	cd $LINE
-# 	cp ../key.txt .
-# 	python ~/code/makePed.py 664-P 767P $LINE 664.vcf
-# 	
-# 	# manually delete grandparents in columns after 4
+# 	LINE=1192
+# 	cd /home/p860v026/temp/3prime/mapped_to_$LINE
+# 	mkdir LM$LINE
+# 	cd LM$LINE
+# 	cp ../../key.txt .
+# 	cp ../../1192clean.vcf .
+# 	python ~/code/makePed.py 1192-P.bam 767-P.bam $LINE 1192clean.vcf
+# # 	
+# # 	# manually delete grandparents in columns after 4
 
 # make clean vcf
 
 	
 # LEP-MAP3
 
-	LINE=664
+	LINE=1192
 
 	module load java
-	cd /home/p860v026/temp/3prime/mapped/$LINE
+	cd /home/p860v026/temp/3prime/mapped_to_$LINE/LM$LINE
 	
 	# make call file
-# 	java -cp ~/bin/lm3/bin ParentCall2 data=$LINE\ped.txt vcfFile=664clean.vcf removeNonInformative=1 > $LINE.call 
+#  	java -cp ~/bin/lm3/bin ParentCall2 data=$LINE\ped.txt vcfFile=$LINE\clean.vcf removeNonInformative=1 > $LINE.call 
 
 	# remove non informative markers
-# 	java -cp ~/bin/lm3/bin Filtering2 data=$LINE.call dataTolerance=0.001 > $LINE\filt.call 
+# 	java -cp ~/bin/lm3/bin Filtering2 data=$LINE.call dataTolerance=0.01 > $LINE\filt.call 
 
-	cat $LINE\filt.call | cut -f 1,2| awk '(NR>=7)' > $LINE\filtsnps.txt
+# 	cat $LINE\filt.call | cut -f 1,2| awk '(NR>=7)' > $LINE\filtsnps.txt
 
 	# separate chromosomes
-	LOD=12 # mixed 15, 16, 19, 10 ok, 11 ok, 12, 11 
+	LOD=19 # mixed 15, 16, 19, 10 ok, 11 ok, 12, 11 
 # 	java -cp ~/bin/lm3/bin SeparateChromosomes2 data=$LINE\filt.call lodLimit=$LOD > $LINE\map$LOD.txt
 
-# 	sort $LINE\map$LOD.txt | uniq -c | sort -n 
+# 	sort $LINE\map$LOD.txt | uniq -c | sort -n > $LINE\_lod$LOD\_lgs.txt
 
 
 
 # 	order markers
 	java -cp ~/bin/lm3/bin OrderMarkers2 grandparentPhase=1 sexAveraged=1 data=$LINE\filt.call map= $LINE\map$LOD.txt > $LINE\order$LOD.txt 
-
+# 
 	awk -vFS="\t" -vOFS="\t" '(NR==FNR){s[NR-1]=$0}(NR!=FNR){if ($1 in s) $1=s[$1];print}' $LINE\filtsnps.txt $LINE\order$LOD.txt > $LINE\order$LOD.mapped
 
 
-#mixed here
+# mixed here
 # 	
 # 
 # 	 
 # export data for plotting 
-#	python ~/code/lm2bpcm.py $LINE $LOD
+python ~/code/lm2bpcm.py $LINE $LOD
 # 	
 # Make plots
-#	module load R
-#	Rscript ~/code/bpcmPlot.r $LINE $LOD
+module load R
+Rscript ~/code/bpcmPlot.r $LINE $LOD
 # 	
-#	tar -zcvf lgs$LINE\_$LOD.tar.gz *.pdf
+tar -zcvf lgs$LINE\_$LOD.tar.gz *.pdf
 # 	
 # 	
 # 	

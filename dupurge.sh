a9e631b0b7c2119d5ae1e7505b4970ba4484f6e9
Veltsos <parisveltsos@gmail.com>
2021-12-30 12:13:08 -0600

    fix python script folder in dupurge and modified for single run for haploid genome

--- dupurge.sh
+++ dupurge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=duprg767              # Job name
+#SBATCH --job-name=duprgv5            # Job name
 #SBATCH --partition=eeb # partition Name (Required) eeb sixhour
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=pveltsos@ku.edu   # Where to send mail 
@@ -10,10 +10,13 @@
 #SBATCH --output=out_purge_%j.log         # Standard output and error log
 
 PURGEDUPSFOLDER=/home/p860v026/bin/purge_dups/bin
-GFOLDER=/home/p860v026/temp/IM767
-GNAME=767.contigs.fasta
-READS=767.fasta.gz
-OUTNAME=767
+SCRIPTSFOLDER=/home/p860v026/bin/purge_dups/scripts
+# GFOLDER=/home/p860v026/temp/IM1192
+GFOLDER=/home/p860v026/temp/genomes/Mgutv5/assembly
+# GNAME=1192.contigs.fasta
+GNAME=MguttatusTOL_551_v5.0.fa
+# READS=1192.fasta.gz
+OUTNAME=v5
 
 module load python
 
@@ -33,50 +36,65 @@ $PURGEDUPSFOLDER/purge_dups -2 -T cutoffs -c PB.base.cov $OUTNAME.split.self.paf
 
 $PURGEDUPSFOLDER/get_seqs -e dups.bed $GNAME  
 
-$PURGEDUPSFOLDER/hist_plot.py PB.stat $OUTNAME
+$SCRIPTSFOLDER/hist_plot.py PB.stat $OUTNAME
 
+
+# Cleanup and move output to purge1 folder
 mkdir purge1
-
-cat purged.fa $GNAME > $OUTNAME\PlusPurged.fasta
+# 
+# # cat purged.fa $GNAME > $OUTNAME\PlusPurged.fasta
+cat hap.fa $GNAME > $OUTNAME\PlusHap.fasta
 mv hap.fa purge1/$OUTNAME\hap1.fa
 mv purged.fa purge1/$OUTNAME\purged1.fa
-mv $OUTNAME.png purge1/$OUTNAME\1.png
 mv calcults.log purge1/$OUTNAME\calcults1.log
 mv purge_dups.log purge1/$OUTNAME\purge_dups1.log
 mv dups.bed purge1/$OUTNAME\dups1.log
-
+ 
 rm $OUTNAME.paf.gz
 rm PB*
 rm cutoffs
 rm *split*
 
+# Make purged histogram
+cd purge1
 
-~/bin/minimap2 -t 12 -xmap-pb $OUTNAME\PlusPurged.fasta $READS | gzip -c - > $OUTNAME\PlusPurged.paf.gz
+~/bin/minimap2 -t 12 -xmap-pb $OUTNAME\purged1.fa ../$READS | gzip -c - > $OUTNAME\PlusHap.paf.gz
+ 
+$PURGEDUPSFOLDER/pbcstat $OUTNAME\PlusHap.paf.gz
 
-$PURGEDUPSFOLDER/pbcstat $OUTNAME\PlusPurged.paf.gz
+$SCRIPTSFOLDER/hist_plot.py PB.stat $OUTNAME\p1
+ 
+cd ..
+ 
+# repeat steps for diploid genomes 
 
-$PURGEDUPSFOLDER/calcuts PB.stat > cutoffs 2>calcults.log
-
-$PURGEDUPSFOLDER/split_fa $OUTNAME\PlusPurged.fasta > $OUTNAME\PlusPurged.split
-
-~/bin/minimap2 -t 12 -xasm5 -DP $OUTNAME\PlusPurged.split $OUTNAME\PlusPurged.split | gzip -c - > $OUTNAME\PlusPurged.split.self.paf.gz
-
-$PURGEDUPSFOLDER/purge_dups -2 -T cutoffs -c PB.base.cov $OUTNAME\PlusPurged.split.self.paf.gz > dups.bed 2> purge_dups.log
-
-$PURGEDUPSFOLDER/get_seqs -e dups.bed $OUTNAME\PlusPurged.fasta   
-
-$PURGEDUPSFOLDER/hist_plot.py PB.stat $OUTNAME\PlusPurged
-
-mkdir purge2
-
-mv hap.fa purge2/$OUTNAME\hap2.fa
-mv purged.fa purge2/$OUTNAME\purged2.fa
-mv $OUTNAME.png purge2/$OUTNAME\purged2.png
-mv calcults.log purge2/$OUTNAME\calcults2.log
-mv purge_dups.log purge2/$OUTNAME\purge_dups2.log
-mv dups.bed purge2/$OUTNAME\dups2.bed
-
-rm $OUTNAME\PlusPurged.paf.gz
-rm PB*
-rm cutoffs
-rm *split*
+# ~/bin/minimap2 -t 12 -xmap-pb $OUTNAME\PlusHap.fasta $READS | gzip -c - > $OUTNAME\PlusHap.paf.gz
+# 
+# $PURGEDUPSFOLDER/pbcstat $OUTNAME\PlusHap.paf.gz
+# 
+# $PURGEDUPSFOLDER/calcuts PB.stat > cutoffs 2>calcults.log
+# 
+# $PURGEDUPSFOLDER/split_fa $OUTNAME\PlusHap.fasta > $OUTNAME\PlusHap.split
+# 
+# ~/bin/minimap2 -t 12 -xasm5 -DP $OUTNAME\PlusHap.split $OUTNAME\PlusHap.split | gzip -c - > $OUTNAME\PlusHap.split.self.paf.gz
+# 
+# $PURGEDUPSFOLDER/purge_dups -2 -T cutoffs -c PB.base.cov $OUTNAME\PlusHap.split.self.paf.gz > dups.bed 2> purge_dups.log
+# 
+# $PURGEDUPSFOLDER/get_seqs -e dups.bed $OUTNAME\PlusHap.fasta   
+# 
+# $SCRIPTSFOLDER/hist_plot.py PB.stat $OUTNAME\PlusHap
+#
+# # Cleanup and move to purge2 folder
+# mkdir purge2
+# 
+# mv hap.fa purge2/$OUTNAME\hap2.fa
+# mv purged.fa purge2/$OUTNAME\purged2.fa
+# mv $OUTNAME.png purge2/$OUTNAME\purged2.png
+# mv calcults.log purge2/$OUTNAME\calcults2.log
+# mv purge_dups.log purge2/$OUTNAME\purge_dups2.log
+# mv dups.bed purge2/$OUTNAME\dups2.bed
+# 
+# rm $OUTNAME\PlusHap.paf.gz
+# rm PB*
+# rm cutoffs
+# rm *split*

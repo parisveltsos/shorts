50e22c9cc9f9e09c628d7902519e38119c00b689
Veltsos <parisveltsos@gmail.com>
2022-02-04 13:25:55 -0600

    minor minimap changes

--- minimap.sh
+++ minimap.sh
@@ -2,33 +2,43 @@
 #SBATCH --job-name=minimap
 #SBATCH --partition=sixhour      
 #SBATCH --time=0-05:59:00        
-#SBATCH --mem-per-cpu=20g         
+#SBATCH --mem-per-cpu=10g  
 #SBATCH --mail-user=pveltsos@ku.edu    
-#SBATCH --ntasks=1                  
+#SBATCH --ntasks=12                  
 #SBATCH --cpus-per-task=1            
 #SBATCH --output=502p_ex.log
 
-REF_FOLDER=/home/p860v026/temp/IM541/make541Sel
-REF_GENOME=541sel.fasta
-REF_GENOME_NAME=$(basename -s .fasta $REF_GENOME)
+REF_FOLDER=/home/p860v026/temp/IM767/purge1
+REF_GENOME=767purged1.fa
+REF_GENOME_NAME=$(basename -s urged1.fa $REF_GENOME)
 
-# QFOLDER=/home/p860v026/temp/Mgutv5/assembly/
-# QFOLDER=/home/p860v026/temp/IM541/make541Sel
-QFOLDER=/home/p860v026/temp/IM502/purge2
+QREAD_FOLDER=/home/p860v026/temp/IM502
 
-QGENOME=502.purged.fasta
-# QGENOME=541sel.fasta
-QGENOME_NAME=$(basename -s .fasta $QGENOME)
+QREADS=$1
+QREADS_NAME=502
+WDIR=502mapping
 
-cd /home/p860v026/temp/minimap
+module load samtools
+
+QGENOME_FOLDER=/home/p860v026/temp/IM502/purge1
+QGENOME=502purged1.fa
+QGENOME_NAME=$(basename -s .fa $QGENOME)
+
+## This is leftover of minimap commands, they need checking
 
 # Genome to genome
 
-# /home/p860v026/bin/minimap2 $QFOLDER/$QGENOME $REF_FOLDER/$REF_GENOME > $REF_GENOME_NAME\_$QGENOME_NAME.paf
+# cd $REF_FOLDER
+# minimap2 -t 12 $QGENOME_FOLDER/$QGENOME $REF_FOLDER/$REF_GENOME > genome_$QGENOME_NAME\_to$REF_GENOME_NAME.paf
 
 
 # Exons to genome
 
-/home/p860v026/bin/minimap2 -ax sr $QFOLDER/$QGENOME All14.v5.exons.fa > exons_$QGENOME_NAME.sam
+# /home/p860v026/bin/minimap2 -ax sr $QREAD_FOLDER/$QGENOME All14.v5.exons.fa > exons_$QGENOME_NAME.sam
 #
-python ~/code/trim.sams.py exons_$QGENOME_NAME
+# python ~/code/trim.sams.py exons_$QGENOME_NAME
+
+
+
+
+
